ad4d2fba42399c5b46c4038c0cfdec06b2f59df6
sudhir yadav <sudhir9297@gmail.com>
2020-08-12 19:07:12 +0530

    bit changes in Header UI

--- src/components/NavBar/navbar.component.jsx
+++ src/components/NavBar/navbar.component.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './navbar.style.css'
+import { FiSearch, FiChevronDown } from "react-icons/fi";
 
 function Navbar() {
 
@@ -7,6 +8,7 @@ function Navbar() {
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
+            console.log(window.scrollY)
             if (window.scrollY > 100) {
                 handleShow(true);
             } else {
@@ -20,8 +22,19 @@ function Navbar() {
 
     return (
         <div className={`nav ${show && "nav_black"}`}>
-            <img className="nav_logo" src={process.env.PUBLIC_URL + '/PictureStudio.png'} alt="MovieHub Logo" />
-            <img className="nav_avatar" src={process.env.PUBLIC_URL + '/avatar_logo.jpg'} alt="avatar Logo" />
+            <div className="left_side">
+                <img className="nav_logo" src={process.env.PUBLIC_URL + '/PictureStudio.png'} alt="MovieHub Logo" />
+            </div>
+            <div className="right_side">
+                <div className="nav_option">
+                    <div className="options">Home</div>
+                    <div className="options">Movies</div>
+                    <div className="options">Series</div>
+                    <div className="options">Mylist  <FiChevronDown style={{ marginLeft: "8px" }} size="22px" onClick={null} /></div>
+                    <div><FiSearch size="22px" onClick={null} /></div>
+                </div>
+                <img className="nav_avatar" src={process.env.PUBLIC_URL + '/avatar_logo.jpg'} alt="avatar Logo" />
+            </div>
         </div>
     )
 }
